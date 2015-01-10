774cda6945a19f8b5d2d89bca8434962e694289b
Tim Churches <timc@Tims-Mac-Pro.local>
2015-01-10 12:05:15 +1100

    Bump to version BT3f

--- braids/settings.cc
+++ braids/settings.cc
@@ -426,7 +426,7 @@ const SettingMetadata Settings::metadata_[] = {
   { 0, 127, "RAT2", rates_values },
   { 0, 0, "CAL.", NULL },
   { 0, 0, "    ", NULL },  // Placeholder for CV tester
-  { 0, 0, "BT3g", NULL },  // Placeholder for version string
+  { 0, 0, "BT3f", NULL },  // Placeholder for version string
 };
 
 /* static */
