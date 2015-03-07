cb0edd6db53a8497c7d790dc8baf3c18de1ff9c9
Tim Churches <timc@Tims-Mac-Pro.local>
2015-03-07 18:00:30 +1100

    Convert env to switch code, replace a division

--- braids/envelope.h
+++ braids/envelope.h
@@ -95,73 +95,136 @@ class Envelope {
          Trigger(static_cast<EnvelopeSegment>(segment_ + 1));
       }
     }
-    if (increment_[segment_]) {
-      if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 0) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 0)) {
-        // exponential
-         value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
-      }
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 1) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 1)) {
-         // linear
-         value_ = Mix(a_, b_, phase_ >> 16);
-      }         
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 2) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 2)) {
-         // wiggly
-         value_ = Mix(a_, b_, Interpolate824(ws_sine_fold, phase_) + 32766);
-      }   
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 3) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 3)) {
-        // close to a sine
-         value_ = Mix(a_, b_, Interpolate824(ws_moderate_overdrive, phase_) + 32766);
-      }   
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 4) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 4)) {
-         // bandwidth-limited square (with rounded corners)
-         value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
-      }   
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 5) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 5)) {
-         // bowing friction LUT - this goes from high to low, hence a_ and b_ switched.
-         value_ = Mix(b_, a_, (Interpolate824(lut_bowing_friction, phase_) - 1) << 1);
-      }   
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 6) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 6)) {
-         // Random target, exponential easing
-        if (phase_ == 0) {
-            b_ = Random::GetWord();
-        }
-        value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
-      }         
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 7) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 7)) {
-         // Random target, linear easing
-        if (phase_ == 0) {
-            b_ = Random::GetWord();
-        }
-        value_ = Mix(a_, b_, phase_ >> 16);
-      }         
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 8) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 8)) {
-         // Random target, square-ish easing
-        if (phase_ == 0) {
-            b_ = Random::GetWord();
-        }
-        value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
-      }         
-      else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 9) ||
-          (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 9)) {
-         // Jump to a random value for the entire phase cycle - causes clicks...
-         if (phase_ == 0) {
-            value_ = Random::GetWord();
-         }
-      }   
-    }
+
+    if (increment_[segment_]) {    
+       if (segment_ == ENV_SEGMENT_ATTACK || segment_ == ENV_SEGMENT_DECAY) {
+          uint8_t type = segment_ == ENV_SEGMENT_ATTACK ? EnvTypeA : EnvTypeD;
+          switch (type) {
+             case 0:
+                // exponential
+                value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
+                break;
+             case 1:
+                // linear
+                value_ = Mix(a_, b_, phase_ >> 16);
+                break;
+             case 2:
+                // wiggly
+                value_ = Mix(a_, b_, Interpolate824(ws_sine_fold, phase_) + 32766);
+                break;
+             case 3:
+                // close to a sine
+                value_ = Mix(a_, b_, Interpolate824(ws_moderate_overdrive, phase_) + 32766);
+                break;
+             case 4:
+                // bandwidth-limited square (with rounded corners)
+                value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
+                break;
+             case 5:
+                // bowing friction LUT - this goes from high to low, hence a_ and b_ switched.
+                value_ = Mix(b_, a_, (Interpolate824(lut_bowing_friction, phase_) - 1) << 1);
+                break;
+             case 6:
+                // Random target, exponential easing
+                if (phase_ == 0) {
+                   b_ = Random::GetWord();
+                }
+                value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
+                break;
+             case 7:
+                // Random target, linear easing
+                if (phase_ == 0) {
+                   b_ = Random::GetWord();
+                }
+                value_ = Mix(a_, b_, phase_ >> 16);
+                break;
+             case 8:
+                // Random target, square-ish easing
+                if (phase_ == 0) {
+                   b_ = Random::GetWord();
+                }
+                value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
+                break;
+             case 9:
+                // Jump to a random value for the entire phase cycle - causes clicks...
+                if (phase_ == 0) {
+                   value_ = Random::GetWord();
+                }
+                break;
+          }
+       }
+    }         
     return value_;
   }
+
+/*    
+//       if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 0) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 0)) {
+//         // exponential
+//          value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
+//       }
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 1) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 1)) {
+//          // linear
+//          value_ = Mix(a_, b_, phase_ >> 16);
+//       }         
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 2) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 2)) {
+//          // wiggly
+//          value_ = Mix(a_, b_, Interpolate824(ws_sine_fold, phase_) + 32766);
+//       }   
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 3) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 3)) {
+//         // close to a sine
+//          value_ = Mix(a_, b_, Interpolate824(ws_moderate_overdrive, phase_) + 32766);
+//       }   
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 4) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 4)) {
+//          // bandwidth-limited square (with rounded corners)
+//          value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
+//       }   
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 5) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 5)) {
+//          // bowing friction LUT - this goes from high to low, hence a_ and b_ switched.
+//          value_ = Mix(b_, a_, (Interpolate824(lut_bowing_friction, phase_) - 1) << 1);
+//       }   
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 6) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 6)) {
+//          // Random target, exponential easing
+//         if (phase_ == 0) {
+//             b_ = Random::GetWord();
+//         }
+//         value_ = Mix(a_, b_, Interpolate824(lut_env_expo, phase_));
+//       }         
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 7) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 7)) {
+//          // Random target, linear easing
+//         if (phase_ == 0) {
+//             b_ = Random::GetWord();
+//         }
+//         value_ = Mix(a_, b_, phase_ >> 16);
+//       }         
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 8) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 8)) {
+//          // Random target, square-ish easing
+//         if (phase_ == 0) {
+//             b_ = Random::GetWord();
+//         }
+//         value_ = Mix(a_, b_, Interpolate824(ws_violent_overdrive, phase_) + 32766);
+//       }         
+//       else if ((segment_ == ENV_SEGMENT_ATTACK && EnvTypeA == 9) ||
+//           (segment_ == ENV_SEGMENT_DECAY && EnvTypeD == 9)) {
+//          // Jump to a random value for the entire phase cycle - causes clicks...
+//          if (phase_ == 0) {
+//             value_ = Random::GetWord();
+//          }
+//       }   
+//     }
+*/
+
+
   
-  inline uint16_t value() const { return value_; }
+ inline uint16_t value() const { return value_; }
 
  private:
   // Phase increments for each segment.
